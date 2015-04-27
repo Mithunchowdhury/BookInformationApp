86b2d4d62ee9a5b6d738d649b1a30b38d7e3ef49
unknown <mithun@mithun-pc.(none)>
2015-04-27 23:52:44 +0600

    Initial Commit eor BookInfo

---
+++ BookInformationApp/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace BookInformationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.isbnTextBox = new System.Windows.Forms.TextBox();
+            this.bookdetaillsTextBox = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.bookdeatilsTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.findButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ISBN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BookDetails";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ISBN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "BOOKdeatils";
+            // 
+            // isbnTextBox
+            // 
+            this.isbnTextBox.Location = new System.Drawing.Point(131, 37);
+            this.isbnTextBox.Name = "isbnTextBox";
+            this.isbnTextBox.Size = new System.Drawing.Size(100, 20);
+            this.isbnTextBox.TabIndex = 4;
+            // 
+            // bookdetaillsTextBox
+            // 
+            this.bookdetaillsTextBox.Location = new System.Drawing.Point(131, 89);
+            this.bookdetaillsTextBox.Name = "bookdetaillsTextBox";
+            this.bookdetaillsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.bookdetaillsTextBox.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(131, 142);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // bookdeatilsTextBox
+            // 
+            this.bookdeatilsTextBox.Location = new System.Drawing.Point(144, 203);
+            this.bookdeatilsTextBox.Name = "bookdeatilsTextBox";
+            this.bookdeatilsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.bookdeatilsTextBox.TabIndex = 7;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(361, 89);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 8;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // findButton
+            // 
+            this.findButton.Location = new System.Drawing.Point(377, 198);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(75, 23);
+            this.findButton.TabIndex = 9;
+            this.findButton.Text = "Find";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 334);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.bookdeatilsTextBox);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.bookdetaillsTextBox);
+            this.Controls.Add(this.isbnTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox isbnTextBox;
+        private System.Windows.Forms.TextBox bookdetaillsTextBox;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox bookdeatilsTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button findButton;
+    }
+}
+
---
+++ BookInformationApp/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookInformationApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private Hashtable tables=new Hashtable();
+
+         private string isbn;
+
+       private string bookDetails;
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            isbn = isbnTextBox.Text;
+
+            bookDetails = bookdetaillsTextBox.Text;
+            
+            tables.Add(isbn, bookDetails);
+            
+            isbnTextBox.Text = "";
+            bookdetaillsTextBox.Text = "";
+
+        }
+
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            string search = textBox3.Text;
+
+ if (tables.ContainsKey(search))
+ {
+     bookdeatilsTextBox.Text = tables[search].ToString();
+
+ }
+ else
+ {
+ MessageBox.Show("Book Not Found");
+ bookdeatilsTextBox.Text = "";
+ }
+
+        }
+    }
+}
